683ebfef4a555279a5ab90167adaf9472d5355cd
Diane Trout <diane@caltech.edu>
2025-08-29 14:59:22 -0700

    Move share-seq test to it's own function
    Since there's two possible templates for hte same list of protocols

--- seqspecgen/tests/test_protocols.py
+++ seqspecgen/tests/test_protocols.py
@@ -40,10 +40,6 @@ class TestProtocols(TestCase):
             ProtocolsIO.ont_library_prep,
             ProtocolsIO.pbmc_cell_prep,
         ]
-        uci_share_seq = [
-            ProtocolsIO.share_seq,
-            ProtocolsIO.pbmc_cell_prep,
-        ]
 
         data = [
             (parse_single_1M_v2, "parse-wt-mega-v2-single-index-libspec-3.yaml.j2"),
@@ -51,9 +47,24 @@ class TestProtocols(TestCase):
             (parse_single_100k_v2, "parse-wt-v2-single-index-libspec-3.yaml.j2"),
             (parse_dual_100k_v2, "parse-wt-v2-dual-index-libspec-3.yaml.j2"),
             (parse_ont_100k_v2, "parse-wt-v2-nanopore.yaml.j2"),
-            (uci_share_seq, "uci-share-seq-rna.yaml.j2"),
         ]
         for p, e in data:
             template = get_template_from_protocols(p, "/assay-terms/OBI_0003109/")
+            self.assertEqual(template, e)
+
+    def test_get_template_from_protocols_share(self):
+        uci_share_seq = [
+            ProtocolsIO.share_seq,
+            ProtocolsIO.pbmc_cell_prep,
+        ]
+        rna = "/assay-terms/OBI_0003109/"
+        atac = "/assay-terms/OBI_0002762/"
+
+        data = [
+            (uci_share_seq, rna, "uci-share-seq-rna.yaml.j2"),
+            (uci_share_seq, atac, "uci-share-seq-atac.yaml.j2"),
+        ]
+        for p, a, e in data:
+            template = get_template_from_protocols(p, a)
             self.assertEqual(template, e)
 
